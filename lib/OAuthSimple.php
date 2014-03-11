a698e5c00cee5476c5412201ddd3ce5f9e70fa7b
Jeremy Watkins <jeremy.r.watkins@gmail.com>
2014-03-11 14:25:16 -0700

    namespace class files

--- lib/OAuthSimple.php
+++ lib/OAuthSimple.php
@@ -1,4 +1,5 @@
-<?php
+<?php namespace XeroAPI\XeroOAuth;
+
 /* OAuthSimple
   * A simpler version of OAuth
   *
--- lib/XeroOAuth.php
+++ lib/XeroOAuth.php
@@ -1,9 +1,4 @@
-<?php
-// Allow OAuthSimple to be autoloaded instead of always including directly.
-// The class_exists() call fires the autoloader.
-if (! class_exists ( 'OAuthSimple' )) {
-	require 'lib/OAuthSimple.php';
-}
+<?php namespace XeroAPI\XeroOAuth;
 
 /**
  * Define a custom Exception for easy trap and detection
